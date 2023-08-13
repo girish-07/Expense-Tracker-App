51ae487c8e3e5ca0dd93005afc217b3d2361ed62
GIRISH S <girish.170902@gmail.com>
2023-08-13 14:52:34 +0530

    Added UI for login and register

---
+++ src/components/Forms/Register.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './Login.css';
+
+class Register extends Component {
+
+    state = {
+        email: '',
+        password: '',
+        displayName: '',
+        fireErrors: ''
+    }
+
+    render () {
+        return (
+            <>
+                <form>
+
+                    <input type="text" 
+                        className="regField"
+                        placeholder="Your Name"
+                        name="name" />
+
+                    <input type="password" 
+                        className="regField"
+                        placeholder="Password"
+                        name="password" />
+                    
+                    <input type="submit"
+                        className="submitBtn"
+                        value="REGISTER" />
+
+                </form>
+            </>
+        )
+    }
+
+}
+
+export default Register;
